92d1d5d2d3a68665de224877e77736fa913ba687
luis coco enriquez <luiscocoenriquez@hotmail.com>
2025-11-29 01:13:04 +0100

    minor changes in the demo title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { RouterOutlet, RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   template: `
-    <h1>Angular Signals - Sections Demo</h1>
+    <h1>Angular Signals - Demo</h1>
     <nav>
       <a routerLink="/section1">1. Intro / Counter</a>
       <a routerLink="/section2">2. What is a Signal</a>
